1899d783a0ea176df69c338ad42738761b4ddad1
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-05-06 01:10:10 -0400

    add help/rules msgs for RPS

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,28 @@
 #!/usr/bin/env node
+import minimist from 'minimist';
+
+const mini = minimist(process.argv.slice(2));
+
+if (mini.h || mini.help) {
+    console.log(`Usage: node-rps [SHOT]
+        Play Rock Paper Scissors (RPS)
+        
+          -h, --help      display this help message and exit
+          -r, --rules     display the rules and exit
+        
+        Examples:
+          node-rps        Return JSON with single player RPS result.
+                          e.g. {"player":"rock"}
+          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                          e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    process.exit(0); 
+}
+
+if (mini.r || mini.rules) {
+    console.log(`Rules for Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`); 
+    process.exit(0); 
+}
