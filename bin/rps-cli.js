e4d61d175ffc68808e53221575a6791135ed6454
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-05-06 16:11:58 -0400

    import minimist; begin fns

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
 import {rps} from "/lib/rpsls";
+import minimist from "minimist";
+
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
 import {rpsls} from "/lib/rpsls";
+import minimist from "minimist";
